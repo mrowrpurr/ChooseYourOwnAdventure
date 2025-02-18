4b14103f1f8197405c55f47d853c84dee0e1aa7f
Mrowr Purr <mrowr.purr@gmail.com>
2025-02-17 20:09:04 -0800

    Switch to xmake (with auto-deploy configured)

---
+++ mod/skse/plugin.cpp
@@ -0,0 +1,39 @@
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+#include <SksePluginDefinition.h>
+
+extern "C" __declspec(dllexport) bool SKSEPlugin_Query(
+    const SKSE ::QueryInterface* a_skse, SKSE ::PluginInfo* a_info
+) {
+    REL::Version version{0, 0, 1};
+    a_info->infoVersion = SKSE ::PluginInfo ::kVersion;
+    a_info->name        = "Choose Your Own Adventure";
+    a_info->version     = version.pack();
+    if (a_skse->IsEditor()) return false;
+    return true;
+}
+
+extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = []() {
+    SKSE ::PluginVersionData v{};
+    v.PluginName("Choose Your Own Adventure");
+    v.AuthorName("Mrowr Purr");
+    v.AuthorEmail("mrowr.purr@gmail.com");
+    v.PluginVersion(REL ::Version(0, 0, 1));
+    v.UsesAddressLibrary();
+    v.UsesUpdatedStructs();
+    return v;
+}();
+
+SKSE_PLUGIN_ENTRY_POINT(const SKSE::LoadInterface* a_skse) {
+    SKSE::Init(a_skse);
+
+    SKSE::GetMessagingInterface()->RegisterListener(
+        "SKSE",
+        [](SKSE::MessagingInterface::Message* a_msg) {
+            if (a_msg->type == SKSE::MessagingInterface::kDataLoaded)
+                RE::ConsoleLog::GetSingleton()->Print("Choose Your Own Adventure initialized.");
+        }
+    );
+
+    return true;
+}
